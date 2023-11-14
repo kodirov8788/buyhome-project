b226bf607cf44b499e0344d2ae558b9be1756da2
SAIDJAHON <saidjahonabdullayev75@gmail.com>
2023-11-14 10:00:58 +0500

    ish bajardim

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Cards from './components/cards/Cards';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <h1>Hello BuyHome</h1>
+      <Cards/>
     </div>
   );
 }
---
+++ src/components/cards/Cards.js
@@ -0,0 +1,200 @@
+import React from "react";
+import OneHouse6 from "../images/6.jpg";
+import OneHouse5 from "../images/5.jpg";
+import OneHouse4 from "../images/4.jfif";
+import OneHouse3 from "../images/3.jfif";
+import OneHouse2 from "../images/2.jpg";
+import OneHouse1 from "../images/1.jpg";
+import { LuBedDouble, LuBath } from "react-icons/lu";
+import { TbTextResize } from "react-icons/tb";
+function Cards() {
+  return (
+    <div className="w-full h-[135vh] border border-black bg-gray-200">
+      <div className="my-12"></div>
+      <div className="w-[89%] h-[120vh]  m-auto my-14 grid grid-cols-3">
+        <div className="w-[420px] h-auto mb-5 mr-5  bg-white">
+          <img className="w-full h-64" src={OneHouse1} alt="" />
+          <div className="flex w-full h-8 text-white">
+            <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+              <h2 className="mx-4">Lovely 3 Bed... </h2>
+            </div>
+            <div className="w-2/5 h-full bg-red-600">
+              <h3 className="text-xl mx-8">$ 340,000</h3>
+            </div>
+          </div>
+          <div className="w-full h-20">
+            <div className="flex gap-28 mx-5 text-lg">
+              <h1>Larnaka , Mazotos</h1>
+              <h1>#LAR42173</h1>
+            </div>
+            <div className="flex gap-36 mx-5 my-12 ">
+              <div className="flex gap-1">
+                <LuBedDouble />
+                <h1>3</h1>
+                <LuBath />
+                <h1>3</h1>
+                <TbTextResize />
+                <h1>167m2</h1>
+              </div>
+              <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                View
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-[420px]  h-auto mb-5 mr-5  bg-white">
+          <img className="w-full h-64" src={OneHouse2} alt="" />
+          <div className="flex w-full h-8 text-white">
+            <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+              <h2 className="mx-4">Lovely 3 Bed... </h2>
+            </div>
+            <div className="w-2/5 h-full bg-red-600">
+              <h3 className="text-xl mx-8">$ 340,000</h3>
+            </div>
+          </div>
+          <div className="w-full h-24">
+            <div className="flex gap-28 mx-5 text-lg">
+              <h1>Larnaka , Mazotos</h1>
+              <h1>#LAR42173</h1>
+            </div>
+            <div className="flex gap-36 mx-5 my-12 ">
+              <div className="flex gap-1">
+                <LuBedDouble />
+                <h1>3</h1>
+                <LuBath />
+                <h1>3</h1>
+                <TbTextResize />
+                <h1>167m2</h1>
+              </div>
+              <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                View
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-[420px] h-auto mb-5 mr-5  bg-white">
+          <img className="w-full h-64" src={OneHouse3} alt="" />
+          <div className="flex w-full h-8 text-white">
+            <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+              <h2 className="mx-4">Lovely 3 Bed... </h2>
+            </div>
+            <div className="w-2/5 h-full bg-red-600">
+              <h3 className="text-xl mx-8">$ 340,000</h3>
+            </div>
+          </div>
+          <div className="w-full h-24">
+            <div className="flex gap-28 mx-5 text-lg">
+              <h1>Larnaka , Mazotos</h1>
+              <h1>#LAR42173</h1>
+            </div>
+            <div className="flex gap-36 mx-5 my-12 ">
+              <div className="flex gap-1">
+                <LuBedDouble />
+                <h1>3</h1>
+                <LuBath />
+                <h1>3</h1>
+                <TbTextResize />
+                <h1>167m2</h1>
+              </div>
+              <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                View
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-[420px]  h-auto mb-5 mr-5  bg-white">
+          <img className="w-full h-64" src={OneHouse4} alt="" />
+          <div className="flex w-full h-8 text-white">
+            <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+              <h2 className="mx-4">Lovely 3 Bed... </h2>
+            </div>
+            <div className="w-2/5 h-full bg-red-600">
+              <h3 className="text-xl mx-8">$ 340,000</h3>
+            </div>
+          </div>
+          <div className="w-full h-24">
+            <div className="flex gap-28 mx-5 text-lg">
+              <h1>Larnaka , Mazotos</h1>
+              <h1>#LAR42173</h1>
+            </div>
+            <div className="flex gap-36 mx-5 my-12 ">
+              <div className="flex gap-1">
+                <LuBedDouble />
+                <h1>3</h1>
+                <LuBath />
+                <h1>3</h1>
+                <TbTextResize />
+                <h1>167m2</h1>
+              </div>
+              <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                View
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-[420px] h-auto mb-5 mr-5  bg-white">
+          <img className="w-full h-64" src={OneHouse5} alt="" />
+          <div className="flex w-full h-8 text-white">
+            <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+              <h2 className="mx-4">Lovely 3 Bed... </h2>
+            </div>
+            <div className="w-2/5 h-full bg-red-600">
+              <h3 className="text-xl mx-8">$ 340,000</h3>
+            </div>
+          </div>
+          <div className="w-full h-24">
+            <div className="flex gap-28 mx-5 text-lg">
+              <h1>Larnaka , Mazotos</h1>
+              <h1>#LAR42173</h1>
+            </div>
+            <div className="flex gap-36 mx-5 my-12 ">
+              <div className="flex gap-1">
+                <LuBedDouble />
+                <h1>3</h1>
+                <LuBath />
+                <h1>3</h1>
+                <TbTextResize />
+                <h1>167m2</h1>
+              </div>
+              <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                View
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="w-[420px]  h-auto mb-5 mr-5  bg-white">
+          <img className="w-full h-64" src={OneHouse6} alt="" />
+          <div className="flex w-full h-8 text-white">
+            <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+              <h2 className="mx-4">Lovely 3 Bed... </h2>
+            </div>
+            <div className="w-2/5 h-full bg-red-600">
+              <h3 className="text-xl mx-8">$ 340,000</h3>
+            </div>
+          </div>
+          <div className="w-full h-24">
+            <div className="flex gap-28 mx-5 text-lg">
+              <h1>Larnaka , Mazotos</h1>
+              <h1>#LAR42173</h1>
+            </div>
+            <div className="flex gap-36 mx-5 my-12 ">
+              <div className="flex gap-1">
+                <LuBedDouble />
+                <h1>3</h1>
+                <LuBath />
+                <h1>3</h1>
+                <TbTextResize />
+                <h1>167m2</h1>
+              </div>
+              <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                View
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;

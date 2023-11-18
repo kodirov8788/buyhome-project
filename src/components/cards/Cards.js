602521e5f7e2bdd3032dfcb82fab1cae9096238e
SAIDJAHON <saidjahonabdullayev75@gmail.com>
2023-11-18 16:16:39 +0500

    ish bajardim

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,18 +1,15 @@
 import React from "react";
-import OneHouse6 from "../images/6.jpg";
-import OneHouse5 from "../images/5.jpg";
-import OneHouse4 from "../images/4.jfif";
-import OneHouse3 from "../images/3.jfif";
-import OneHouse2 from "../images/2.jpg";
-import OneHouse1 from "../images/1.jpg";
 import { LuBedDouble, LuBath } from "react-icons/lu";
 import { TbTextResize } from "react-icons/tb";
+import { card } from "../../satatic_data";
+import { Link } from "react-router-dom";
 function Cards() {
   return (
     <div className="w-full h-[135vh] border border-black bg-gray-200">
       <div className="my-12"></div>
       <div className="w-[89%] h-[120vh]  m-auto my-14 grid grid-cols-3">
-        <div className="w-[420px] h-auto mb-5 mr-5  bg-white">
+        
+        {/* <div className="w-[420px] h-auto mb-5 mr-5  bg-white">
           <img className="w-full h-64" src={OneHouse1} alt="" />
           <div className="flex w-full h-8 text-white">
             <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
@@ -41,8 +38,9 @@ function Cards() {
               </button>
             </div>
           </div>
-        </div>
-        <div className="w-[420px]  h-auto mb-5 mr-5  bg-white">
+        </div> */}
+
+        {/* <div className="w-[420px]  h-auto mb-5 mr-5  bg-white">
           <img className="w-full h-64" src={OneHouse2} alt="" />
           <div className="flex w-full h-8 text-white">
             <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
@@ -191,7 +189,40 @@ function Cards() {
               </button>
             </div>
           </div>
-        </div>
+        </div> */}
+
+        {card.map((item) => (
+          <div key={item.id} className="w-[420px] h-auto mb-5 mr-5  bg-white">
+            <img className="w-full h-64" src={item.img} alt="" />
+            <div className="flex w-full h-8 text-white">
+              <div className="w-3/5 h-full bg-gray-700 text-base flex items-center">
+                <h2 className="mx-4">{item.title}... </h2>
+              </div>
+              <div className="w-2/5 h-full bg-red-600">
+                <h3 className="text-xl mx-8">$ {item.price},000</h3>
+              </div>
+            </div>
+            <div className="w-full h-24">
+              <div className="flex gap-28 mx-5 text-lg">
+                <h1>{item.address}</h1>
+                <h1>#{item.homeId} </h1>
+              </div>
+              <div className="flex gap-36 mx-5 my-12 ">
+                <div className="flex gap-1">
+                  <LuBedDouble />
+                  <h1>{item.bed}</h1>
+                  <LuBath />
+                  <h1>{item.bathroom}</h1>
+                  <TbTextResize />
+                  <h1>{item.land}m2</h1>
+                </div>
+                <button className="hover:bg-red-600 w-24 h-8 text-white rounded-sm bg-gray-700">
+                  View
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/satatic_data.js
@@ -0,0 +1,188 @@
+import OneHouse6 from "./components/images/6.jpg";
+import OneHouse5 from "./components/images/5.jpg";
+import OneHouse4 from "./components/images/4.jfif";
+import OneHouse3 from "./components/images/3.jfif";
+import OneHouse2 from "./components/images/2.jpg";
+import OneHouse1 from "./components/images/1.jpg";
+
+
+
+export const card = [
+  {
+    id: 1,    
+    img: OneHouse1,
+    image: [
+      {
+        id: 1,
+        img: OneHouse1,
+      },
+      {
+        id: 2,
+        img: OneHouse2,
+      },
+      {
+        id: 3,
+        img: OneHouse3,
+      },
+      {
+        id: 4,
+        img: OneHouse4,
+      },
+    ],
+    title: "Lovely 3 Bed",
+    price:340,
+    address:"Larnaka , Mazotos",
+    homeId:"LAR42173",
+    bed:3,
+    bathroom:3,
+    land:167,
+  },
+  {
+    id: 1,    
+    img: OneHouse2,
+    image: [
+      {
+        id: 1,
+        img: OneHouse1,
+      },
+      {
+        id: 2,
+        img: OneHouse2,
+      },
+      {
+        id: 3,
+        img: OneHouse3,
+      },
+      {
+        id: 4,
+        img: OneHouse4,
+      },
+    ],
+    title: "Deluxe Villa ",
+    price:695,
+    address:"Larnaka , Dhekelia",
+    homeId:"LAR42120",
+    bed:4,
+    bathroom:2,
+    land:258,
+  },
+  {
+    id: 1,    
+    img: OneHouse3,
+    image: [
+      {
+        id: 1,
+        img: OneHouse1,
+      },
+      {
+        id: 2,
+        img: OneHouse2,
+      },
+      {
+        id: 3,
+        img: OneHouse3,
+      },
+      {
+        id: 4,
+        img: OneHouse4,
+      },
+    ],
+    title: "Unique Plot I",
+    price:480,
+    address:"Larnaka , St George",
+    homeId:"LAR41809",
+    bed:4,
+    bathroom:4,
+    land:521,
+  },
+  
+  {
+    id: 1,    
+    img: OneHouse4,
+    image: [
+      {
+        id: 1,
+        img: OneHouse1,
+      },
+      {
+        id: 2,
+        img: OneHouse2,
+      },
+      {
+        id: 3,
+        img: OneHouse3,
+      },
+      {
+        id: 4,
+        img: OneHouse4,
+      },
+    ],
+    title: "Investment –",
+    price:2750,
+    address:"Limassol , Pyrgos",
+    homeId:"LIM40468",
+    bed:3,
+    bathroom:3,
+    land:246.5,
+  },
+  
+  {
+    id: 1,    
+    img: OneHouse5,
+    image: [
+      {
+        id: 1,
+        img: OneHouse1,
+      },
+      {
+        id: 2,
+        img: OneHouse2,
+      },
+      {
+        id: 3,
+        img: OneHouse3,
+      },
+      {
+        id: 4,
+        img: OneHouse4,
+      },
+    ],
+    title: "5 Bedroom H",
+    price:849,
+    address:"Larnaka , Mazotos",
+    homeId:"LAR40282",
+    bed:5,
+    bathroom:5,
+    land:420,
+  },
+  
+  {
+    id: 6,    
+    img: OneHouse1,
+    image: [
+      {
+        id: 1,
+        img: OneHouse1,
+      },
+      {
+        id: 2,
+        img: OneHouse2,
+      },
+      {
+        id: 3,
+        img: OneHouse3,
+      },
+      {
+        id: 4,
+        img: OneHouse4,
+      },
+    ],
+    title: "Odysseas Villa",
+    price:3022,
+    address:"Larnaka , Pervolia",
+    homeId:"LAR40177",
+    bed:5,
+    bathroom:5,
+    land:676,
+  },
+];

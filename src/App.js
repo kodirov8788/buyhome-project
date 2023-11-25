a70c1a6fc704592c7a9cb25cf4fc15a4528c5ff8
SAIDJAHON <saidjahonabdullayev75@gmail.com>
2023-11-25 15:16:45 +0500

    added new change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import { Link } from 'react-router-dom';
-import './App.css';
-<<<<<<< HEAD
-import Cards from './components/cards/Cards';
-
-function App() {
-  return (
-    <div className="App">
-      <Cards/>
-=======
-import Single from './single/Single';
-import { fake_data } from './static_data';
-
-
+import { Link } from "react-router-dom";
+import Cards from "./components/cards/Cards";
+import "./App.css";
 
 function App() {
   return (
     <div className="w-full h-full">
-      {fake_data.map(addr => (
+      <Cards />
+
+      {/* {fake_data.map(addr => (
         <Link to={`single/${addr.id}/swiper`} state={addr}>  <h1>{addr.address}</h1></Link>
-      ))}
-
-      {/* <Single /> */}
-
-
->>>>>>> bd6b04469a0f9548f1f696539043d4798793d4f7
+      ))} */}
     </div>
   );
 }
